7e1ff018e009d794bf23889f0ba5c7c2089b9e15
nuzwub <nuzwub@protonmail.com>
2024-05-09 16:41:43 -0400

    Pared back to ulta-simple.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dadjokes
 A dad jokes API in Python with FastAPI
+
+NOTES:
+When getting the dev environment up and running, re-launch the virtual environment...
+source venv/bin/activate
+uvicorn jokeapi:app --reload
--- jokeapi.py
+++ jokeapi.py
@@ -3,15 +3,10 @@
 """Basic API using FastAPI"""
  
 # main.py
- 
+
 from by_id import by_id
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
-from new import new_joke
-from pydantic import BaseModel
-from typing import Annotated
- 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+from fastapi import FastAPI
+from pydantic import BaseModel 
  
 class Joke(BaseModel):
     setup: str
@@ -19,7 +14,7 @@ class Joke(BaseModel):
     tags: str | None = None
  
 app = FastAPI()
- 
+
 @app.get("/")
 async def root():
     return {"setup": "What does a baby computer call his father?", "punchline": "Data.", "tags": ""}
@@ -29,7 +24,3 @@ async def byId(id: str):
     the_joke = by_id(id)
     return {the_joke}
  
-@app.post("/new/")
-async def new(joke: Joke, token: Annotated[str, Depends(oauth2_scheme)]):
-    the_joke = new_joke(setup=joke.setup, punchline=joke.punchline, tags=joke.tags)
-    return {the_joke}
